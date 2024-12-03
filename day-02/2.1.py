723ea4d75a26d8bfe87f2b2592f8850c86f96d21
Jordan Fox <jordan.fox.1996@gmail.com>
2024-12-03 12:18:42 +1100

    Upload day-02

---
+++ day-02/2.1.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append("../")
+from get_input import get_input
+
+input = get_input(2).split("\n")
+count = 0
+
+def check_direction(numbers):
+    if int(numbers[0]) > int(numbers[1]):
+        direction = "down"
+    else:
+        direction = "up"
+    return direction
+
+def check_if_safe(direction, numbers):
+    x = 0
+    safe = True
+    if direction == "down": 
+        while x < (len(numbers) - 1) and safe == True:
+            if int(numbers[x]) < int(numbers[x+1]): # If next number ascends
+                safe = False
+            else:
+                diff = abs(int(numbers[x]) - int(numbers[x+1]))
+                if diff < 1 or diff > 3:
+                    safe = False
+            x += 1
+
+    if direction == "up":
+        while x < (len(numbers) - 1) and safe == True:
+            if int(numbers[x]) > int(numbers[x+1]): # If next number descends
+                safe = False
+            else:
+                diff = abs(int(numbers[x]) - int(numbers[x+1]))
+                if diff < 1 or diff > 3:
+                    safe = False
+            x += 1
+    return safe
+
+for line in input:
+    numbers = line.rstrip().split(" ")
+    direction = check_direction(numbers)
+    if check_if_safe(direction, numbers):
+        count += 1
+
+print(count)
